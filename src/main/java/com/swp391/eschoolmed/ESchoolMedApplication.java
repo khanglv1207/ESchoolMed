5e46df77919fef55af43095746c57dd874199b77
khanglv1207 <khanglv1207@gmail.com>
2025-05-18 08:57:07 +0700

    first commit

---
+++ src/main/java/com/swp391/eschoolmed/ESchoolMedApplication.java
@@ -0,0 +1,13 @@
+package com.swp391.eschoolmed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ESchoolMedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ESchoolMedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/swp391/eschoolmed/ESchoolMedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swp391.eschoolmed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ESchoolMedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
