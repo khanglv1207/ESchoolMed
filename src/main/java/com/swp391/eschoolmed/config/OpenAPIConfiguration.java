2cf5fe5953b99b16b2691f4d07ee3137dad9b0a9
khanglv1207 <khanglv1207@gmail.com>
2025-06-11 21:05:19 +0700

    change code

---
+++ src/main/java/com/swp391/eschoolmed/config/OpenAPIConfiguration.java
@@ -0,0 +1,35 @@
+package com.swp391.eschoolmed.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.servers.Server;
+
+@Configuration
+@SecurityScheme(
+        name = "Bearer Authentication",
+        type = SecuritySchemeType.HTTP,
+        bearerFormat = "JWT",
+        scheme = "bearer",
+        description = "A JWT token is required to access this API. JWT token can be obtain by api/accounts/login " +
+                "} API"
+)
+public class OpenAPIConfiguration {
+    @Value("${springdoc.servers}")
+    private String serverUrl;
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new io.swagger.v3.oas.models.info.Info().title("REST API").version("v3"))
+                .addServersItem(new Server().url(serverUrl).description("API Gateway"))
+                .addSecurityItem(new SecurityRequirement().addList("Bearer Authentication"));
+    }
+
+
+}
---
+++ src/main/java/com/swp391/eschoolmed/dto/response/IntrospectResponse.java
@@ -0,0 +1,14 @@
+package com.swp391.eschoolmed.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class IntrospectResponse {
+    private boolean valid;
+}
