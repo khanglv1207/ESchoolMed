48d9696b79c214bb57c7c5f25749565bf45b7a69
khanglv1207 <khanglv1207@gmail.com>
2025-06-25 00:24:55 +0700

    add cros

--- src/main/java/com/swp391/eschoolmed/config/SecurityConfig.java
+++ src/main/java/com/swp391/eschoolmed/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.swp391.eschoolmed.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -25,7 +26,8 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests(auth -> auth
+        http.cors(Customizer.withDefaults())
+                .authorizeHttpRequests(auth -> auth
                 // cho phép tat cả
                 .requestMatchers(HttpMethod.POST, "/api/users/login").permitAll()
                 .requestMatchers(HttpMethod.GET, "/api/mail/receive_email").permitAll()
