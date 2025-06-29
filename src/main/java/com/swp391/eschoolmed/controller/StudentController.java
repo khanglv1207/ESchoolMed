df8e8600c7f672e47f6b4c29694b9c388ac4d7ba
khanglv1207 <khanglv1207@gmail.com>
2025-06-29 16:28:58 +0700

    add profile student

--- src/main/java/com/swp391/eschoolmed/controller/StudentController.java
+++ src/main/java/com/swp391/eschoolmed/controller/StudentController.java
@@ -1,17 +1,16 @@
 package com.swp391.eschoolmed.controller;
 
+import com.swp391.eschoolmed.dto.ApiResponse;
+import com.swp391.eschoolmed.dto.request.ParentProfileRequest;
+import com.swp391.eschoolmed.dto.request.StudentProfileRequest;
 import com.swp391.eschoolmed.repository.StudentRepository;
-import com.swp391.eschoolmed.service.StudentImportService;
+import com.swp391.eschoolmed.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
-import org.springframework.http.MediaType;
 
 @RestController
 @RequestMapping("/api/students")
@@ -20,16 +19,27 @@ public class StudentController {
     @Autowired
     private StudentRepository studentRepository;
     @Autowired
-    private StudentImportService studentImportService;
+    private StudentService studentService;
 
     @PostMapping(value = "/import-student", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public ResponseEntity<String> importExcel(@RequestParam("file") MultipartFile file) {
         try {
-            studentImportService.importExcel(file);
+            studentService.importExcel(file);
             return ResponseEntity.ok("Thêm học sinh vào danh sách thành công");
         } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Thêm học sinh thất bại" + e.getMessage());
         }
     }
+
+
+    @PostMapping("/profile-student")
+    public ApiResponse<Void> profileStudent(@RequestBody StudentProfileRequest request){
+        studentService.studentProfile(request);
+        return ApiResponse.<Void>builder()
+                .message("Cập nhật thông tin học sinh thành công.")
+                .result(null)
+                .build();
+    }
+
 
 }
