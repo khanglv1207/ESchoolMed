819f4af97e2e2dba385b5542429d86e61b4dd5d6
khanglv1207 <khanglv1207@gmail.com>
2025-06-29 20:39:06 +0700

    fix profile

--- src/main/java/com/swp391/eschoolmed/dto/request/UpdateParentProfileRequest.java
+++ src/main/java/com/swp391/eschoolmed/dto/request/UpdateParentProfileRequest.java
@@ -5,11 +5,12 @@ import lombok.Data;
 import java.util.UUID;
 
 @Data
-public class ParentProfileRequest {
+public class UpdateParentProfileRequest {
     private UUID userid;
     private String fullName;
     private String email;
     private String phoneNumber;
     private String address;
     private String dateOfBirth;
+
 }
