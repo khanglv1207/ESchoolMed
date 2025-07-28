3f045b7b7cf01cccaab9ea34e6e5704390c1d670
khanglv1207 <khanglv1207@gmail.com>
2025-07-28 23:16:42 +0700

    fix vaccination 1

--- src/main/java/com/swp391/eschoolmed/repository/StudentRepository.java
+++ src/main/java/com/swp391/eschoolmed/repository/StudentRepository.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
@@ -12,7 +13,14 @@ import com.swp391.eschoolmed.model.Student;
 
 @Repository
 public interface StudentRepository extends JpaRepository<Student, UUID> {
-
+    @Query("""
+    SELECT s FROM Student s
+    WHERE s.studentId NOT IN (
+        SELECT hc.student.studentId
+        FROM HealthProfile hc
+        WHERE hc.vaccinationRecord LIKE %:vaccineName%
+    )
+""")
     List<Student> findStudentsNotVaccinatedWith(@Param("vaccineName") String vaccineName);
     Optional<Student> findByStudentCode(String studentCode);
 
--- src/main/java/com/swp391/eschoolmed/repository/VaccinationNotificationRepository.java
+++ src/main/java/com/swp391/eschoolmed/repository/VaccinationNotificationRepository.java
@@ -8,6 +8,6 @@ import java.util.List;
 import java.util.UUID;
 
 public interface VaccinationNotificationRepository extends JpaRepository<VaccinationNotification, UUID> {
-    List<VaccinationResult> findAllByFinalizedFalse();
+
 
 }
